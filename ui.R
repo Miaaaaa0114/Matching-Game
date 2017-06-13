a442b0a1bbf18c251e20364682004d87e2ffef41
Miaaaaa0114 <svl5815@psu.edu>
2017-06-13 18:05:31 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,91 +1,98 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
+library(shinythemes)
 library(shinyDND)
-shinyUI(
+shinyUI(fluidPage(
+  h1("Matching Game of Histogram "),
+  verbatimTextOutput("text1"),
+  fluidRow(
+    column(3,
+           h3("A"),
+      plotOutput("Boxplot1")),
+    column(3,
+            h3("B"),
+        plotOutput("Boxplot2")),
+    column(3,
+           h3("C"),
+        plotOutput("Boxplot3")),
+    column(3,
+           h3("D"),
+        plotOutput("Boxplot4"))
+    ),
+  fluidRow(
+    column(3,
+           div(dragUI("div5","A", style = "background-color:red", 
+                      class = "dragelement"), style = "margin-left:-70%")
+    ),
+    column(3,
+           div(dragUI("div4","B", style = "background-color:red", 
+                      class = "dragelement"), style = "margin-left:-40%")
+    ),   
+    column(3,
+           div(dragUI("div3","C", style = "background-color:red", 
+                      class = "dragelement"), style = "margin-left:40%")
+    ),    
+    column(3,
+           div(dragUI("div2","D", style = "background-color:red", 
+                      class = "dragelement"), style = "margin-left:70%")
+    )
+  ),
+  fluidRow(
+    column(3,
+           hr(),
+           dropUI("div7", row_n = 1, col_n = 1,style = "background-color:yellow")),
+    column(3,
+           hr(),
+           dropUI("div8", row_n = 1, col_n = 1,style = "background-color:pink")),
+    column(3,
+           hr(),
+           dropUI("div9", row_n = 1, col_n = 1,style = "background-color:yellow")),
+    column(3,
+           hr(),
+           dropUI("div6", row_n = 1, col_n = 1,style = "background-color:pink"))
+      
+    ),
+  fluidRow(
+    column(3,
+      plotOutput("Histplot1")
+    ),
+    column(3,
+      plotOutput("Histplot2")
+           ),
+    column(3,
+           plotOutput("Histplot3")
+           ),
+    column(3,
+           plotOutput("Histplot4"))
+  ),
+  fluidRow(
+    column(3,
+           verbatimTextOutput('out1')),
+    column(3,
+           verbatimTextOutput('out2')),
+    column(3,
+           verbatimTextOutput('out3')),
+    column(3,
+           verbatimTextOutput('out4'))
+  ),
+  
+  fluidRow(
+    column(8,
+           sidebarPanel(actionButton("hhhhh", "Check")),
+           mainPanel("Answer & Points:", verbatimTextOutput("numPoint"))),
+    column(4,
+           h3("Please check answers before going to next level!")),
   fluidPage(
-    h1("Matching Game of Histogram and Boxplot"),
-    h2("Dragging the red letter of boxplot to the box of histogram"),
-    fluidRow(
-      column(3,
-             h3("A"),
-             list(tags$head(tags$style()), 
-                  HTML('<img src="boxplot1.png", height= 200px"    
-                    style="float:right"/>','<p style="color:red"></p>'))
-                  ),
-      column(3,
-             h3("B"),
-             list(tags$head(tags$style()), 
-                  HTML('<img src="boxplot3.png", height= 200px"    
-                       style="float:right"/>','<p style="color:red"></p>'))
-                  ),
-      column(3,
-             h3("C"),
-             list(tags$head(tags$style()), 
-                  HTML('<img src="boxplot4.png", height= 200px"    
-                    style="float:right"/>','<p style="color:red"></p>'))
-                ),
-      column(3,
-             h3("D"),
-             list(tags$head(tags$style()), 
-                  HTML('<img src="boxplot2.png", height= 200px"    
-                    style="float:right"/>','<p style="color:red"></p>'))
-      )
-    ),
-    fluidRow(
-      column(3,
-             div(dragUI("div5","A", style = "background-color:red", 
-                        class = "dragelement"), style = "margin-left:-70%")
-      ),
-      column(3,
-             div(dragUI("div4","B", style = "background-color:red", 
-                        class = "dragelement"), style = "margin-left:-40%")
-      ),   
-      column(3,
-             div(dragUI("div3","C", style = "background-color:red", 
-                        class = "dragelement"), style = "margin-left:40%")
-      ),    
-      column(3,
-             div(dragUI("div2","D", style = "background-color:red", 
-                        class = "dragelement"), style = "margin-left:70%")
-      )
-    ),
-    fluidRow(
-      column(3,
-             hr(),
-             dropUI("div3", row_n = 1, col_n = 1,style = "background-color:yellow")),
-      column(3,
-             hr(),
-             dropUI("div3", row_n = 1, col_n = 1,style = "background-color:pink")),
-      column(3,
-             hr(),
-             dropUI("div3", row_n = 1, col_n = 1,style = "background-color:yellow")),
-      column(3,
-             hr(),
-             dropUI("div3", row_n = 1, col_n = 1,style = "background-color:pink"))
-    ),
-    fluidRow(
-      column(3,
-             list(tags$head(tags$style()), 
-                  HTML('<img src="hist1.png", height= 200px"    
-                       style="float:right"/>','<p style="color:red"></p>'))
-                  ),
-      column(3,
-             list(tags$head(tags$style()), 
-                  HTML('<img src="hist2.png", height= 200px"    
-                       style="float:right"/>','<p style="color:red"></p>'))
-                  ),
-      column(3,
-             list(tags$head(tags$style()), 
-                  HTML('<img src="hist3.png", height= 200px"    
-                       style="float:right"/>','<p style="color:red"></p>'))
-                  ),
-      column(3,
-             list(tags$head(tags$style()), 
-                  HTML('<img src="hist4.png", height= 200px"    
-                       style="float:right"/>','<p style="color:red"></p>'))
-                  )
-      ),
-    sidebarPanel(actionButton("clear", "Show Answer")),
-    fluidRow(
-    mainPanel("Answer:", verbatimTextOutput("numPoint")))
-    
-))
+           actionButton("clear", "Next Level"),
+           verbatimTextOutput("numPoints"))
+    )
+)
+)
